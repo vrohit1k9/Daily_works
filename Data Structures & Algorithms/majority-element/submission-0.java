d90786ba0efacef9f28e6f398d33c02efcb2809b
vrohit1k9 <vrohith1009@gmail.com>
2026-09-12 19:10:36 +0530

    Add: majority-element - submission-0

---
+++ Data Structures & Algorithms/majority-element/submission-0.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int maxele=0;
+        int maxfreq=0;
+        HashMap<Integer,Integer> map=new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            map.put(nums[i],map.getOrDefault(nums[i],0)+1);
+            if(map.get(nums[i])>maxfreq){
+                maxfreq=map.get(nums[i]);
+                maxele=nums[i];
+            }
+        }
+        return maxele;
+
+    }
+}
